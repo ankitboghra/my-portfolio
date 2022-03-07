39e7d49999fbe6a6c9a57b567edd67f54ec3280a
ankitboghra <ankitboghra@gmail.com>
2022-03-07 15:02:48 +0530

    fix: captcha issue

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-836d91b86c609169ad66.js"
+    "url": "webpack-runtime-2b51dfc0f38d40ffbdbb.js"
   },
   {
     "url": "framework-ec6ebe3a11ad92d00b85.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f083a3d7967a492a0e9ae8a9c7c61c30"
+    "revision": "9efd5d1f1656a2eaaeecd8e1f7fe6a55"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
